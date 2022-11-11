2563163348a56bda5bbd864c4e09569b8b62f342
Thanh Tra <82217333+meofiscoding@users.noreply.github.com>
2022-11-12 06:22:02 +0700

    update home screen

--- Teleparty/Teleparty/Pages/Index.cshtml.cs
+++ Teleparty/Teleparty/Pages/Index.cshtml.cs
@@ -1,11 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.Text.Json;
 
 namespace Teleparty.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+        public class Result
+        {
+            public bool adult { get; set; }
+            public string backdrop_path { get; set; }
+            public int id { get; set; }
+            public string title { get; set; }
+            public string original_language { get; set; }
+            public string original_title { get; set; }
+            public string overview { get; set; }
+            public string poster_path { get; set; }
+            public string media_type { get; set; }
+            public List<int> genre_ids { get; set; }
+            public double popularity { get; set; }
+            public string release_date { get; set; }
+            public bool video { get; set; }
+            public double vote_average { get; set; }
+            public int vote_count { get; set; }
+        }
+
+        public class Root
+        {
+            public int page { get; set; }
+            public List<Result> results { get; set; }
+            public int total_pages { get; set; }
+            public int total_results { get; set; }
+        }
+
+        [BindProperty]
+        public List<Result> Movie { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -16,7 +48,7 @@ namespace Teleparty.Pages
         {
             var client = new HttpClient();
             var request = new HttpRequestMessage();
-            request.RequestUri = new Uri("https://ophimmoi.net/category/chieu-rap/");
+            request.RequestUri = new Uri("https://api.themoviedb.org/3/trending/movie/week?api_key=5a626560913d4cbd29ba9b8ba06221ad&page=1");
             request.Method = HttpMethod.Get;
 
             request.Headers.Add("Accept", "*/*");
@@ -24,7 +56,9 @@ namespace Teleparty.Pages
 
             var response = await client.SendAsync(request);
             var result = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(result);
+
+            var listmovies = JsonConvert.DeserializeObject<Root>(result);
+            Movie = listmovies.results;
         }
     }
 }
---
+++ Teleparty/Teleparty/Pages/Party.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Teleparty.Pages
+{
+    public class PartyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
